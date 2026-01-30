4beea9db98a085ca9a5d0fdef352af34fbe1df20
SzymCode <szymon.radomski@yahoo.com>
2026-01-30 19:07:30 +0100

    chore: remove unused components from build

--- nuc_templates.ts
+++ nuc_templates.ts
@@ -2,12 +2,7 @@ import type { App } from 'vue'
 
 import {
   NucAuthors,
-  NucCardBox,
-  NucCardBoxes,
-  NucCardCategory,
   NucCube,
-  NucFlipCard,
-  NucHexagonsButton,
   NucHomeLink,
   NucTiles,
 } from '.'
@@ -15,12 +10,7 @@ import {
 export function registerNucTemplates(app: App<Element>): void {
   app
     .component('nuc-authors', NucAuthors)
-    .component('nuc-card-box', NucCardBox)
-    .component('nuc-card-boxes', NucCardBoxes)
-    .component('nuc-card-category', NucCardCategory)
     .component('nuc-cube', NucCube)
-    .component('nuc-flip-card', NucFlipCard)
-    .component('nuc-hexagons-button', NucHexagonsButton)
     .component('nuc-home-link', NucHomeLink)
     .component('nuc-tiles', NucTiles)
 }
